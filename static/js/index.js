437cc6de5721481dbc8ef86b8e320853f8ea8250
Danyow.Ed <i.zuucol@gmail.com>
2019-03-30 15:01:12 +0800

    3. For loops

--- static/js/index.js
+++ static/js/index.js
@@ -3,8 +3,15 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!!!!!!!',
         seen: true,
-    }
+        todos: [
+            {text: '1'},
+            {text: '2'},
+            {text: '3'},
+            {text: '4'},
+        ],
+    },
 });
 
 app.message = 'I have changed the data!';
 app.seen = false;
+app.todos.push({text: 'New item'})
