78a975af9c07c02f25a21ce2db0b9c7cc1e84c6f
Danyow <i.zuucol@gmail.com>
2019-04-01 08:33:30 +0800

    提交二阶段

--- static/js/index.js
+++ static/js/index.js
@@ -1,32 +1,98 @@
-Vue.component('todo-item', {
-    props:['todo'],
-    template: '<li> {{ todo.text }} </li>'
+// Vue.component('todo-item', {
+//     props:['todo'],
+//     template: '<li> {{ todo.text }} </li>'
+// })
+
+// var app = new Vue({
+//     el: '#app',
+//     data: {
+//         message: 'Hello Vue!!!!!!!',
+//         seen: true,
+//         todos: [
+//             {text: '1'},
+//             {text: '2'},
+//             {text: '3'},
+//             {text: '4'},
+//         ],
+//         groceryList: [
+//             {id: 0, text:'Vegetables'},
+//             {id: 1, text:'Cheese'},
+//             {id: 2, text:'Whatever else humans are supposed to eat'},
+//         ]
+//     },
+//     methods: {
+//         reverseMessage: function () {
+//             this.message = this.message.split('').reverse().join('')
+//         }
+//     },
+// });
+
+// app.message = 'I have changed the data!';
+// app.seen = false;
+// app.todos.push({text: 'New item'})
+
+
+"use strict"
+
+// emojify returns the corresponding emoji image
+function emojify(name) {
+	var out = `<img src="/static/emojis/` + name + `.png">`
+	return out
+}
+
+Vue.component("swatch", {
+	props: ["active", "swatch", "effect"],
+	template: `
+		<div class="grid-item">
+			<div class="grid-cell--top" :style="effect(swatch)" >
+				<span
+					v-html="swatch.emoji"
+					:class="{ bounce: swatch == active }"
+				></span>
+			</div>
+			<div class="grid-cell--bottom" :style="corrected_color()">
+				{{ swatch.color.toUpperCase() }}
+			</div>
+		</div>
+	`, 
+    methods: {
+        corrected_color: function () {
+            return {
+                color: this.swatch.color == '#ffffff' ? '#000000' : this.swatch.color
+            }
+        }
+    }
 })
 
 var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!!!!!!!',
-        seen: true,
-        todos: [
-            {text: '1'},
-            {text: '2'},
-            {text: '3'},
-            {text: '4'},
-        ],
-        groceryList: [
-            {id: 0, text:'Vegetables'},
-            {id: 1, text:'Cheese'},
-            {id: 2, text:'Whatever else humans are supposed to eat'},
-        ]
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
-    },
-});
-
-app.message = 'I have changed the data!';
-app.seen = false;
-app.todos.push({text: 'New item'})
+	el: "#app",
+	data: {
+		active: "",
+		swatches: [
+		    { emoji: emojify("lion"    ), color: "#ff691f" },
+		    { emoji: emojify("tiger"   ), color: "#fab81e" },
+		    { emoji: emojify("fish"    ), color: "#7fdbb6" },
+		    { emoji: emojify("frog"    ), color: "#19cf86" },
+		    { emoji: emojify("dolphin" ), color: "#91d2fa" },
+		    { emoji: emojify("whale"   ), color: "#1b95e0" },
+		    { emoji: emojify("elephant"), color: "#abb8c2" },
+		    { emoji: emojify("octopus" ), color: "#e81c4f" },
+		    { emoji: emojify("pig"     ), color: "#f58ea8" },
+		    { emoji: emojify("unicorn" ), color: "#981ceb" },
+		    { emoji: emojify("rabbit"  ), color: "#ffffff" },
+		    { emoji: emojify("wolf"    ), color: "#000000" },
+		],
+	},
+	methods: {
+		// activate actives a swatch (emoji/color)
+		activate: function (swatch) {
+			this.active = swatch
+		},
+		// gradient returns a precomposed gradient
+		gradient: function (swatch) {
+			return {
+				background: "linear-gradient(100deg, whitesmoke -100%, " + swatch.color + ")",
+			}
+		}
+	}
+})
